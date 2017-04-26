02606fcd58c503f21dc922aa81d532323b9567e9
Ryan Nutt <ryan@classcube.com>
2017-04-26 08:42:03 -0500

    Added social media icons to the links panel;

--- inc/pages/admin-links.php
+++ inc/pages/admin-links.php
@@ -2,9 +2,24 @@
 if (!defined('ABSPATH')) {
     return; 
 }
-?>
 
-<h3>Links</h3>
-<p>
-    Here are some links back to ClassCube
-</p>
+$image_dir = plugins_url('img', dirname(__DIR__));
+
+?>
+<div class="cc-social">
+    <a href="https://github.com/ClassCube/wordpress-lti-consumer">
+        <img src="<?php echo $image_dir; ?>/github-32px.png">
+    </a>
+    <a href="https://www.facebook.com/ClassCube/">
+        <img src="<?php echo $image_dir; ?>/facebook-32px.png">
+    </a>
+    <a href="https://twitter.com/classcube/">
+        <img src="<?php echo $image_dir; ?>/twitter-32px.png">
+    </a>
+    <a href="https://www.linkedin.com/company/16245650/">
+        <img src="<?php echo $image_dir; ?>/linkedin-32px.png">
+    </a>
+    <a href="https://www.youtube.com/channel/UCni3wW88-dLjVrMjWf8eG4A">
+        <img src="<?php echo $image_dir; ?>/youtube-32px.png">
+    </a>
+</div>
